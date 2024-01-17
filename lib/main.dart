2d518fc03570252a051d27612937af9c53b44f06
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2024-01-18 00:35:16 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:news_app/Config/Theme.dart';
+
+import 'Pages/HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'News App',
+      theme: lightTheme,
+      themeMode: ThemeMode.system,
+      darkTheme: darkTheme,
+      home: HomePage(),
+    );
+  }
+}
